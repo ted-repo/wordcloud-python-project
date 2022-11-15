bd1784c25613b0c9db14e2257780a9fa32e41d14
Ted <57218901+ted-repo@users.noreply.github.com>
2022-11-15 13:30:00 +0100

    saving changes

--- final2.py
+++ final2.py
@@ -129,3 +129,24 @@ def calculate_frequencies(file_contents):
 # Testing the function
 calculate_frequencies('demo.txt')
 
+
+
+
+
+##########saving code from page:
+
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words you can use to process your text
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+    "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
+    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
+    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+    
+    # LEARNER CODE START HERE
+    
+    #wordcloud
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies()
+    return cloud.to_array()
